40a7fdcc837675ee9a0a08a3fc0489a78ac49615
Anissa Molina <ani_luz15@yahoo.com>
2024-01-25 10:39:52 -0800

    AHHHHHH

--- calc.js
+++ calc.js
@@ -1,8 +1,37 @@
-function add() {
-    let num = prompt("Enter");
-    let sum = 0;
-    if (num === true) {
-        sum = num + num
-        console.log("Sum")
-    }
+let op = prompt("Enter operation, + - * /");
+let num1 = parseFloat(document.getElementById(num1));
+let num2 = parseFloat(document.getElementById(num2));
+
+
+switch (op){
+    case '+':
+        alert(`${num1} + ${num2} = ${num1 + num2}`);
+    break;
+    
+    case '-':
+        alert(`${num1} - ${num2} = ${num1 - num2}`);
+    break;
+
+    case `*`:
+        alert(`${num1} * ${num2} = ${num1 * num2}`);
+    break;
 }
+
+// let op = prompt("Enter operation, + - * /");
+// let num1 = parseFloat(prompt("Enter a number"));
+// let num2 = parseFloat(prompt("Enter a second number"));
+
+
+// switch (op){
+//     case '+':
+//         alert(`${num1} + ${num2} = ${num1 + num2}`);
+//     break;
+    
+//     case '-':
+//         alert(`${num1} - ${num2} = ${num1 - num2}`);
+//     break;
+
+//     case `*`:
+//         alert(`${num1} * ${num2} = ${num1 * num2}`);
+//     break;
+// }
