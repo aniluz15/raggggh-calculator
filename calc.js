42d406beec3dd82108eee7f2416f8472dbd10a5a
Anissa Molina <ani_luz15@yahoo.com>
2024-01-25 19:28:05 -0800

    math === bad

--- calc.js
+++ calc.js
@@ -1,6 +1,6 @@
 let op = prompt("Enter operation, + - * /");
-let num1 = parseFloat(document.getElementById(num1));
-let num2 = parseFloat(document.getElementById(num2));
+let num1 = parseFloat(prompt("Enter a number"));
+let num2 = parseFloat(prompt("Enter a second number"));
 
 
 switch (op){
@@ -15,23 +15,7 @@ switch (op){
     case `*`:
         alert(`${num1} * ${num2} = ${num1 * num2}`);
     break;
+
+    case `/`:
+        alert(`${num1} / ${num2} = ${num1 / num2}`);
 }
-
-// let op = prompt("Enter operation, + - * /");
-// let num1 = parseFloat(prompt("Enter a number"));
-// let num2 = parseFloat(prompt("Enter a second number"));
-
-
-// switch (op){
-//     case '+':
-//         alert(`${num1} + ${num2} = ${num1 + num2}`);
-//     break;
-    
-//     case '-':
-//         alert(`${num1} - ${num2} = ${num1 - num2}`);
-//     break;
-
-//     case `*`:
-//         alert(`${num1} * ${num2} = ${num1 * num2}`);
-//     break;
-// }
